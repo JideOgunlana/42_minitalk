f3a61d99023bd30918401e3ee67fd77cb24d34f9
Jide_O <45870109+JideOgunlana@users.noreply.github.com>
2022-08-02 18:03:46 +0200

    Update server.c

--- server.c
+++ server.c
@@ -6,15 +6,15 @@
 /*   By: bogunlan <bogunlan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 16:41:28 by bogunlan          #+#    #+#             */
-/*   Updated: 2022/08/01 17:47:18 by bogunlan         ###   ########.fr       */
+/*   Updated: 2022/08/02 12:27:31 by bogunlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minitalk.h"
 
-t_server *val;
+t_server	*val;
 
-int ft_isblank(char c)
+int	ft_isblank(char c)
 {
 	if (c <= 32)
 		return (1);
@@ -23,8 +23,8 @@ int ft_isblank(char c)
 
 int		isvalid(char c, int base)
 {
-	char digits[17] = "0123456789abcdef";
-	char digits2[17] = "0123456789ABCDEF";
+	char	digits[17] = "0123456789abcdef";
+	char	digits2[17] = "0123456789ABCDEF";
 
 	while (base--)
 		if (digits[base] == c || digits2[base] == c)
@@ -60,45 +60,48 @@ int		ft_atoi_base(const char *str, int str_base)
 
 void	handle_sig(int signum)
 {
-	// int	c;
+	int	c;
 
-	// val->bit += 1;
-	// if (signum == SIGUSR1)
-	// 	val->messg[val->counter] = '0';
-	// else if (signum == SIGUSR2)
-	// 	val->messg[val->counter] = '1';
-	// if (val->bit == 8)
-	// {
-	// 	c = ft_atoi_base(val->messg, 2);
-	// 	write(STDOUT_FILENO, &c, 1);
-	// 	val->bit = 0;
-	// }
-	// val->counter++;
-	static int	c = 0;
-	static int	bits = 0;
-	int			i;
-	int			n;
-
+	val->bit += 1;
 	if (signum == SIGUSR1)
+		val->messg[val->counter] = '0';
+	else if (signum == SIGUSR2)
+		val->messg[val->counter] = '1';
+	if (val->bit == 8)
 	{
-		i = 1;
-		n = 1;
-		while (i++ < (8 - bits))
-			n *= 2;
-		c += n;
+		c = ft_atoi_base(val->messg, 2);
+		if (c == 0)
+			ft_printf("true");
+		write(STDOUT_FILENO, &c, 1);
+		val->bit = 0;
 	}
-	bits++;
-	if (bits == 8)
-	{
-		ft_printf("%c", 255 - c);
-		bits = 0;
-		c = 0;
-	}
+	val->counter++;
+	// ft_printf("%d\n", val->counter);
+	// static int	c = 0;
+	// static int	bits = 0;
+	// int			i;
+	// int			n;
+
+	// if (signum == SIGUSR1)
+	// {
+	// 	i = 1;
+	// 	n = 1;
+	// 	while (i++ < (8 - bits))
+	// 		n *= 2;
+	// 	c += n;
+	// }
+	// bits++;
+	// if (bits == 8)
+	// {
+	// 	ft_printf("%c", 255 - c);
+	// 	bits = 0;
+	// 	c = 0;
+	// }
 }
 
-int main()
+int	main(void)
 {
-	void	(*sig_handler_return) (int);
+	void	(*sig_handler_return)(int);
 	int		server_pid;
 
 	server_pid = getpid();
